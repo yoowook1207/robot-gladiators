f5836672f597c7915c021d8a3cc1ce51c0e19504
Sung Yoo <yoowook1207@gmail.com>
2021-11-29 17:50:37 -0500

    first form of the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,42 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function fight() {
+    window.alert("Welcome to Robot Gladiators!");
+
+enemyHealth = enemyHealth - playerAttack;
+
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+//check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!")
+    }
+    else {
+        window.alert(enemyName + " still has " +enemyHealth + " health left.")
+}
+
+playerHealth = playerHealth - enemyAttack;
+
+console.log(
+    enemyName + " attacked " + playerName + ". " +playerName + " now has " + playerHealth + " health remaining.");
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + "has died!");
+    }
+    else {
+        window.alert(playerName + " still has" + playerHealth + " health left.")
+    }
+
+}
+
+fight();
